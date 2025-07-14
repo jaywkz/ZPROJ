a2044b528c25e2966a53be0a8db5a5d8a64c79f4
JAY <jakeybink@gmail.com>
2025-07-13 23:20:57 -0400

    Camera and Physics Added

--- ZPROJ.py
+++ ZPROJ.py
@@ -4,7 +4,9 @@ import time
 import os
 import sys
 import json
+import math
 import pygame
+import copy
 
 pygame.init()
 
@@ -23,14 +25,17 @@ PATHS = {
     "missing_texture":"missing.png"
 }
 
+STARTER_RESOLUTION = (1280,720)
+INTERVAL = 1 # just don't  change this i have no fucking clue what it does
+
 # Code Begins 
 
-# Constants
+# Runtime Constants
 
 IMAGELOADING = pygame.image
 SOUND = pygame.mixer.Sound
 
-SCREEN = pygame.display.set_mode((500,500))
+SCREEN = pygame.display.set_mode(STARTER_RESOLUTION)
 EVENT = pygame.event
 GROUP = pygame.sprite.Group
 TRANSFORM = pygame.transform
@@ -41,6 +46,7 @@ DISPLAY = pygame.display
 STORAGE = json.load(open(PATHS["storage"],"r"))
 
 NPC_STATS = STORAGE["Stats"]["npc"]
+REGULAR_STATS = STORAGE["Stats"]["regular"]
 SOLID_COLORS = STORAGE["Solid_Colors"]
 BACKGROUND_COLOR = SOLID_COLORS["background"]
 
@@ -48,11 +54,20 @@ BACKGROUND_COLOR = SOLID_COLORS["background"]
 
 Running = True
 
-Active_Game_Objects = GROUP()
+Sprite_Groups = {}
 Keys_Pressed = []
+Objects_Hovered = []
+
+Running_Groups = []
+Connected_Inputs = {}
+Gamerules:dict = STORAGE["Default_Gamerules"]
 
 Cam_X = 0
 Cam_Y = 0
+
+Active_Resolution = STARTER_RESOLUTION
+Previous_Time = time.time()
+Deltatime = 0
 
 Assets = {}
 
@@ -94,26 +109,54 @@ class sprite(pygame.sprite.Sprite):
         self.image = TRANSFORM.scale(loaded_surface,self.size)
         self.rect = self.image.get_rect()
 
-    def __init__(self,coordinates:tuple=(0,0)):
-        # This has all the default sprite functions. The attributes are loaded separately under each sprite class' init.
+    def __init__(self,coordinates:tuple=(0,0),regular_sprite:bool=False,custom_params:dict={}):
+        # This has all the default sprite functions.
         super().__init__()
         self.x:float = coordinates[0]
         self.y:float = coordinates[1]
-        self.stat_table:dict # Define stat table so it's not annoying
-        self.sprite_attributes:dict = self.stat_table["sprite_attributes"]
+        if regular_sprite:
+            self.sprite_attributes = copy.deepcopy(REGULAR_STATS)  
+        else:
+            self.stat_table:dict
+            self.sprite_attributes:dict = self.stat_table["sprite_attributes"]
+        for name,param in custom_params.items():
+                if name in self.sprite_attributes.keys():
+                    self.sprite_attributes[name] = param
         self.size:tuple = tuple(self.sprite_attributes["default_size"])
-        self.scrolling_object:bool = self.sprite_attributes["can_scroll"]
+        self.scrolling_locked:bool = not self.sprite_attributes["can_scroll"]
+        self.velocity:list = [0,0]
+        self.object_friction:float = Gamerules["Default_Object_Friction"]
+        self.anchored:bool = self.sprite_attributes["start_anchored"]
+        self.visible:bool = self.sprite_attributes["start_visible"]
         # Run any initialization functions
         self.update_image(self.sprite_attributes["default_sprite"])
 
+    def draw_pos(self):
+        self.rect.centerx = (self.x if self.scrolling_locked else self.x - Cam_X) + Active_Resolution[0] / 2
+        self.rect.centery = -(self.y if self.scrolling_locked else self.y - Cam_Y) + Active_Resolution[1] / 2
+        
     def update(self):
-        self.rect.centerx = self.x - Cam_X if self.scrolling_object else 0
-        self.rect.centery = self.y - Cam_Y if self.scrolling_object else 0
-    
+        interval_ratio = Deltatime/INTERVAL
+        if not self.anchored: # Physics crap
+            friction = self.object_friction
+            minimum_vel = Gamerules["Velocity_Minimum"]
+            velocity = self.velocity
+            vel_x,vel_y = velocity[0],velocity[1]
+            applied_friction = 1 - (friction * interval_ratio)
+            x,y = vel_x * applied_friction, vel_y * applied_friction
+            self.x += vel_x - x
+            self.y += vel_y - y
+            self.velocity = [x,y]
+            for n,c in enumerate(self.velocity):
+                if abs(c) < minimum_vel:
+                    self.velocity[n] = 0
+            
     # These are hook methods called during runtime
     def mouse_down(self):
         pass 
     def mouse_hovered(self):
+        pass
+    def mouse_stop_hovered(self):
         pass
 
 class npc(sprite):
@@ -121,47 +164,146 @@ class npc(sprite):
         # Gather Stats
         self.stat_table:dict = NPC_STATS[sprite_type] # HARD-TIED TABLE CONSTANT TO NPC CLASS
         self.npc_stats:dict = self.stat_table["npc_attributes"]
-        super().__init__(coordinates=coordinates) # Initiate Default Sprite Behavior stat table is set
+        super().__init__(coordinates=coordinates) # Initiate Default Sprite Behavior once stat table is set
 
         self.npc_name = sprite_type
         self.npc_type = self.npc_stats["npc_type"]
     
     def mouse_hovered(self):
-        print(f"Mouse hover on {self.npc_name}")
+        while self in Objects_Hovered:
+            pass
+    
+    def update(self):
+        super().update()
+
+# Input Functions
+
+def move_cam(amount:tuple):
+    for n,c in enumerate(Camera.velocity):
+        upd_vel = c + amount[n]
+        if not abs(upd_vel) > Gamerules["Camera_Maximum_Speed"]:
+            Camera.velocity[n] = upd_vel
+
+class input_connecter():
+    def __init__(self,functions:dict,hold:bool):
+        self.functions:dict = functions
+        self.hold:bool = hold
+    def activate(self):
+        funcs = self.functions
+        for func in funcs.keys():
+            params = funcs[func]
+            func(**params)
 
 # Active Runtime
+
+# Loading Stuff
+
+Sprite_Groups["game_objects"] = GROUP()
+Sprite_Groups["menu_objects"] = GROUP()
+
+Game_Objects = Sprite_Groups["game_objects"]
+Menu_Objects = Sprite_Groups["menu_objects"]
 
 Assets["missing"] = IMAGELOADING.load(rf"{PATHS["assets"]}\{PATHS["missing_texture"]}")
 load_assets()
 
-gunner = npc("gunner")
-Active_Game_Objects.add(gunner)
+Camera = sprite(regular_sprite=True,custom_params={"default_sprite":"camera_icon"})
+Camera.object_friction = Gamerules["Camera_Friction"]
+Camera.visible = False
 
-while Running:
+# Startup Functionality
+
+gunner = npc("gunner")
+gunner.velocity = [250,-250]
+Game_Objects.add(gunner)
+Game_Objects.add(Camera)
+
+Running_Groups.append("game_objects")
+
+# Build camera directions
+
+camera_speed = Gamerules["Camera_Speed"]
+
+keybinds = Gamerules["Keybinds"]
+camera_construct = {
+    keybinds["cam_forward"]:[0,1],
+    keybinds["cam_left"]:[-1,0],
+    keybinds["cam_back"]:[0,-1],
+    keybinds["cam_right"]:[1,0],}
+
+for key,value in camera_construct.items():
+    connecter = input_connecter({move_cam:{"amount":[value[0]*camera_speed,value[1]*camera_speed]}},True)
+    Connected_Inputs[key] = connecter
+
+# Frame-by-Frame Runtime
+
+while Running: # Frame-By-Frame Loop, Each iteration is a literal frame
+    # Get Deltatime
+    current_time = time.time()
+    Deltatime = current_time - Previous_Time
+    Previous_Time = current_time
+    # Get Active Sprites
+    active_groups,active_objects = [],[]
+    for group_name in Sprite_Groups.keys(): # Make list of active sprites
+        if group_name in Running_Groups:
+            group:GROUP = Sprite_Groups[group_name]
+            active_groups.append(group)
+            active_objects.extend(group.sprites())    
+    # Do Inputs
     inputs = EVENT.get()
-    active_objects = Active_Game_Objects.sprites()
-    for event in inputs:
+    for event in inputs: # Iterate Through Inputs
         if event.type == pygame.QUIT:
             Running = False
             break
         else:
             event_dict:dict = event.dict
-            if event.type == pygame.KEYDOWN:
-                Keys_Pressed.append(event_dict["unicode"])
-            elif event.type == pygame.KEYUP:
-                Keys_Pressed.remove(event_dict["unicode"])
-            elif event.type == pygame.MOUSEMOTION:
-                mouse_coordinate:tuple = event_dict["pos"]
+            event_type:int = event.type
+            # Mouse Hovered
+            if event_type == pygame.MOUSEMOTION:
+                mouse_pos = event_dict["pos"]
+                x,y = mouse_pos[0],mouse_pos[1]
                 for object in active_objects:
                     object:sprite
-                    if RECT.collidepoint(object.rect,mouse_coordinate[0],mouse_coordinate[1]):
-                        object.mouse_hovered()
-                    
-                
-    Active_Game_Objects.update()
-    SCREEN.fill(BACKGROUND_COLOR)
-    Active_Game_Objects.draw(SCREEN)
+                    if RECT.collidepoint(object.rect,x,y):
+                        if not object in Objects_Hovered:
+                            Objects_Hovered.append(object)
+                            object.mouse_hovered()
+                    elif object in Objects_Hovered:
+                        Objects_Hovered.remove(object)
+                        object.mouse_stop_hovered()
+            # Key Pressed
+            elif event_type == pygame.KEYDOWN:
+                if "unicode" in event_dict:
+                    key = event_dict["unicode"]
+                    Keys_Pressed.append(key)
+                    if key in Connected_Inputs:
+                        connecter:input_connecter = Connected_Inputs[key]
+                        if not connecter.hold:
+                            connecter.activate()
+            # Key stopped being pressed
+            elif event_type == pygame.KEYUP:
+                    if "unicode" in event_dict:
+                        key = event_dict["unicode"]
+                        Keys_Pressed.remove(key)
+    # Do Connections attached to gotten inputs
+    for key in Keys_Pressed:
+        if key in Connected_Inputs:
+            connecter:input_connecter = Connected_Inputs[key]
+            if connecter.hold:
+                connecter.activate()
+                print(connecter.functions[move_cam]["amount"])
+    # Position Global Camera variables
+    Cam_X = Camera.x
+    Cam_Y = Camera.y
+    SCREEN.fill(BACKGROUND_COLOR) # Draw Frame
+    for group in active_groups:
+        for spr in group.sprites():
+            spr:sprite
+            spr.update()
+            if spr.visible:
+                spr.draw_pos()
+                SCREEN.blit(spr.image,spr.rect)
     DISPLAY.flip()
     
 pygame.quit()
-    
+
